c74864d3217c2dea4ac302b1b003039162ea9124
Zahid <zahid4317@gmail.com>
2022-09-23 22:47:47 +0500

    Added hover to get started button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,27 @@
 import { Box, Button, Heading, Text, Flex, Stack } from "@chakra-ui/react";
 import { List, ListItem, ListIcon } from "@chakra-ui/react";
 import { MdCheckCircle, MdSettings } from "react-icons/md";
-import { Container } from '@chakra-ui/react'
+import { Container } from "@chakra-ui/react";
 
 const Card = () => {
   return (
     <>
       <Flex m="20px">
         <Box
-          maxW={["400px","600px","994px"]}
+          maxW={["400px", "600px", "994px"]}
           maxH={[null, null, "400px"]}
           margin="auto"
           mt="-120px"
           borderRadius="12px"
           overflow="hidden"
           boxShadow="10px 14px 10px rgba(0, 0, 0, 0.2)"
-
         >
           <Flex direction={["column", "column", "row"]}>
             <Box bg="#F0EAFB" p="60px" textAlign="center">
               <Text fontWeight="800" fontSize="26px" color="#171923">
                 Premium PRO
               </Text>
-              <Heading as="h3" fontSize={["40px","50px", "60px"]} mt="16px">
+              <Heading as="h3" fontSize={["40px", "50px", "60px"]} mt="16px">
                 $329
               </Heading>
               <Text color="#171923" fontSize="18px" fontWeight="500px" mt="8px">
@@ -33,14 +32,18 @@ const Card = () => {
                 borderRadius="8px"
                 h="51px"
                 mt="24px"
-                w={["200px","220px","282px"]}
+                w={["200px", "220px", "282px"]}
+                _hover={{
+                  background: "white",
+                  color: "teal.500",
+                }}
               >
                 Get Started
               </Button>
             </Box>
 
             <Flex>
-              <Box p="60px" fontSize={["","","16px"]} bg="white">
+              <Box p="60px" fontSize={["", "", "16px"]} bg="white">
                 <Stack>
                   <Text>
                     Access these features when you get this pricing package for
